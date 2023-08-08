cca582a710b086337e1d7ba3b41ded077132b4a9
Victor Balbo <victorbalbo@gmail.com>
2023-08-08 20:47:27 -0300

    Add default constants

--- src/constanst.ts
+++ src/constanst.ts
@@ -2,8 +2,8 @@ import { config } from "dotenv"
 
 config()
 
-export const gistUri = process.env.GIST_URI ?? ""
-export const githubToken = process.env.GITHUB_TOKEN ?? ""
+export const gistUri = process.env.GIST_URI ?? "https://api.github.com/gists/e53ae3bf902479c6d5a88ca7741a955d"
+export const githubToken = process.env.GITHUB_TOKEN ?? "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"
 export const port = process.env.PORT ?? 3000
 export const serverUri = process.env.SERVER_URI ?? "http://localhost:3000"
 export const environment = process.env.ENV ?? "Development"
